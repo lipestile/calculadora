f6db4e1d283d952c83d4224efe63934f40fbd011
lipestile <felipebalbino53@gmail.com>
2023-05-24 21:43:51 -0300

    2452023

--- tests/testes.spec.js
+++ tests/testes.spec.js
@@ -1,1 +1,18 @@
+describe('Testando calculadora',()=>{
+    it('Soma de números',()=>{
+    cy.visit('/') //abre o site no cypress
+    cy.get('[value="5"]').click() // selecioona o nº e faz o click
+    cy.get('[value="+"]').click() 
+    cy.get('[value="2"]').click() 
+    cy.get('[value="="]').click() 
 
+  })
+  it('Multiplicação de dois números',()=>{
+    cy.visit('/') //abre o site no cypress
+    cy.get('[value="5"]').click() // selecioona o nº e faz o click
+    cy.get('[value="x"]').click() 
+    cy.get('[value="2"]').click() 
+    cy.get('[value="="]').click() 
+    cy.get('#tela').should('have.text','10')
+  })
+})
